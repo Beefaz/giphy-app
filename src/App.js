eed4df07891bbc91ca6c4e25af315a8ba97ab35f
Beefaz <justinas.geryba@gmail.com>
2022-02-06 20:59:03 +0200

    fixed build issues

--- src/App.js
+++ src/App.js
@@ -10,25 +10,24 @@ import {setGifs} from "./redux/slices/gifsSlice";
 function App() {
   const dispatch = useDispatch();
 
-  const fetchGifs = async () => {
-    await fetch('https://api.giphy.com/v1/gifs/trending?api_key=f5hp3qYikfuNpEWmJfYECuJ5EmGMK7dZ&limit=12&rating=g')
-      .then((response) => response.json())
-      .then((response) => {
-        const cleanedGifs = filterObjectProperties(
-          response.data,
-          ['id', 'images', 'import_datetime']
-        );
-        const sortedGifs = sortByDateParam(cleanedGifs, 'import_datetime');
-        dispatch(setGifs(sortedGifs));
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  };
-
   useEffect(() => {
+    const fetchGifs = async () => {
+      await fetch('https://api.giphy.com/v1/gifs/trending?api_key=f5hp3qYikfuNpEWmJfYECuJ5EmGMK7dZ&limit=12&rating=g')
+        .then((response) => response.json())
+        .then((response) => {
+          const cleanedGifs = filterObjectProperties(
+            response.data,
+            ['id', 'images', 'import_datetime']
+          );
+          const sortedGifs = sortByDateParam(cleanedGifs, 'import_datetime');
+          dispatch(setGifs(sortedGifs));
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    };
     fetchGifs();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="App">
